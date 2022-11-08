47ba7158b13f6234287ca7a640faedc0dd33dfde
MicheleRai <rainonemichele1999@gmail.com>
2022-11-08 17:12:14 +0100

    object arry added

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ const images = [
 BONUS 1:
 Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 BONUS 2:
-Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.*/
 
 
 
@@ -40,77 +40,101 @@ Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
 
 const arrImages = [
-	'img/01.jpg',
-	'img/02.jpg',
-	'img/03.jpg',
-	'img/04.jpg',
-	'img/05.jpg',
+	{
+		image: '01.webp',
+		title: "Marvel's Spiderman Miles Morale",
+		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+	},
+	{
+		image: '02.webp',
+		title: 'Ratchet & Clank: Rift Apart',
+		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+	},
+	{
+		image: '03.webp',
+		title: 'Fortnite',
+		text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+	},
+	{
+		image: '04.webp',
+		title: 'Stray',
+		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+	},
+	{
+		image: '05.webp',
+		title: "Marvel's Avengers",
+		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+	},
 ];
 
-const eleSlider = document.querySelector('.slider');
-const eleBtnUp = document.querySelector('.btn-up');
-const eleBtnDown = document.querySelector('.btn-down');
 
+const eleSliderViewer = document.querySelector('.slider-viewer');
+const eleSliderThumbs = document.querySelector('.thumbs');
+const eleBtnLeft = document.querySelector('.btn-left');
+const eleBtnRight = document.querySelector('.btn-right');
 
+// creare i tag immagine nell'html
 for (let i = 0; i < arrImages.length; i++) {
-	const eleImg = document.createElement('img');
-	eleImg.src = arrImages[i];
+	// creare i tag immagine che va nella sezione grande .slider-viewer
+	const eleImg = document.createElement('div');
+	eleImg.innerHTML =`<img src="img/${arrImages[i].image}" alt="">`;
 	eleImg.classList.add('slider-img');
-
 	if (i === 0) {
 		eleImg.classList.add('active');
 	}
+	eleSliderViewer.append(eleImg);
 
-	eleSlider.append(eleImg);
+	// creare i tag immagine che vanno nella sezione .thumbs
+	const eleThumb = document.createElement('div');
+	eleThumb.innerHTML =`<img src="img/${arrImages[i].image}" alt="">`;
+	eleThumb.classList.add('thumb-img');
+	if (i === 0) {
+		eleThumb.classList.add('active');
+	}
+	eleSliderThumbs.append(eleThumb);
 }
 
-const listEleImg = document.querySelectorAll('.slider-img'); 
+const listEleImg = document.querySelectorAll('.slider-img'); // non e' un array ma qualcosa di simile
+const listThumbs = document.querySelectorAll('.thumb-img');
 
 let activeIndex = 0;
+document.body.style.backgroundImage = `url('${arrImages[activeIndex]}')`;
+document.body.style.backgroundSize = 'cover';
 
-// const firstClone = listEleImg[0].cloneNode(true);
-// const lastClone = listEleImg[listEleImg.length - 1].cloneNode(true);
-
-// firstClone.id = 'first-clone';
-// lastClone.id = 'last-clone';
-
-// eleSlider.append(firstClone);
-// eleSlider.prepend(lastClone);
-
-
-
-eleBtnDown.addEventListener('click', function () {
-	
+// aggiungere gli event listeners ai due bottoni
+eleBtnRight.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
 	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
 
-    if (activeIndex === (arrImages.length - 1)) {
-        activeIndex = 0;
-    }else{
+	// incrementare l'active index con reset per slider infinito
 	activeIndex++;
-    }
+	if (activeIndex === listEleImg.length) {
+		activeIndex = 0;
+	}
 
+	// aggiungere la classe active all'elemento successivo
 	listEleImg[activeIndex].classList.add('active');
-
-	// eleBtnUp.classList.remove('hidden');
-	// if (activeIndex === listEleImg.length - 1) {
-	// 	eleBtnDown.classList.add('hidden');
-	// }
+	listThumbs[activeIndex].classList.add('active');
+	document.body.style.backgroundImage = `url('${arrImages[activeIndex]}')`;
+	document.body.style.backgroundSize = 'cover';
 });
 
-eleBtnUp.addEventListener('click', function () {
-	
+eleBtnLeft.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
 	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
 
-    if (activeIndex === 0) {
-        activeIndex = (arrImages.length - 1)
-    }else{
+	// decrementare l'active index con reset per slider infinito
+	if (activeIndex === 0) {
+		activeIndex = listEleImg.length;
+	}
 	activeIndex--;
-    }
 
+	// aggiungere la classe active all'elemento successivo
 	listEleImg[activeIndex].classList.add('active');
-
-	// eleBtnDown.classList.remove('hidden');
-	// if (activeIndex === 0) {
-	// 	eleBtnUp.classList.add('hidden');
-	// }
+	listThumbs[activeIndex].classList.add('active');
+	document.body.style.backgroundImage = `url('${arrImages[activeIndex]}')`;
+	document.body.style.backgroundSize = 'cover';
 });
+
