a575e0520adfa4785fc2fe8a67568f20f973e2e9
MicheleRai <rainonemichele1999@gmail.com>
2022-11-08 16:34:22 +0100

    old carousel added

--- js/script.js
+++ js/script.js
@@ -31,3 +31,86 @@ BONUS 1:
 Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 BONUS 2:
 Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+
+
+
+
+
+//vecchio
+
+
+const arrImages = [
+	'img/01.jpg',
+	'img/02.jpg',
+	'img/03.jpg',
+	'img/04.jpg',
+	'img/05.jpg',
+];
+
+const eleSlider = document.querySelector('.slider');
+const eleBtnUp = document.querySelector('.btn-up');
+const eleBtnDown = document.querySelector('.btn-down');
+
+
+for (let i = 0; i < arrImages.length; i++) {
+	const eleImg = document.createElement('img');
+	eleImg.src = arrImages[i];
+	eleImg.classList.add('slider-img');
+
+	if (i === 0) {
+		eleImg.classList.add('active');
+	}
+
+	eleSlider.append(eleImg);
+}
+
+const listEleImg = document.querySelectorAll('.slider-img'); 
+
+let activeIndex = 0;
+
+// const firstClone = listEleImg[0].cloneNode(true);
+// const lastClone = listEleImg[listEleImg.length - 1].cloneNode(true);
+
+// firstClone.id = 'first-clone';
+// lastClone.id = 'last-clone';
+
+// eleSlider.append(firstClone);
+// eleSlider.prepend(lastClone);
+
+
+
+eleBtnDown.addEventListener('click', function () {
+	
+	listEleImg[activeIndex].classList.remove('active');
+
+    if (activeIndex === (arrImages.length - 1)) {
+        activeIndex = 0;
+    }else{
+	activeIndex++;
+    }
+
+	listEleImg[activeIndex].classList.add('active');
+
+	// eleBtnUp.classList.remove('hidden');
+	// if (activeIndex === listEleImg.length - 1) {
+	// 	eleBtnDown.classList.add('hidden');
+	// }
+});
+
+eleBtnUp.addEventListener('click', function () {
+	
+	listEleImg[activeIndex].classList.remove('active');
+
+    if (activeIndex === 0) {
+        activeIndex = (arrImages.length - 1)
+    }else{
+	activeIndex--;
+    }
+
+	listEleImg[activeIndex].classList.add('active');
+
+	// eleBtnDown.classList.remove('hidden');
+	// if (activeIndex === 0) {
+	// 	eleBtnUp.classList.add('hidden');
+	// }
+});
